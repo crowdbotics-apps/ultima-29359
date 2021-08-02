43a6c132476293e9635b9fdb2d2a13fe3ae11b24
Crowdbotics <team@crowdbotics.com>
2021-08-02 23:49:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ultima_29359Identifier",
+  url: "https://ultima_29359Identifier.botics.co",
+  api: "https://ultima_29359Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
